619a9538281ba4de5823df7b3d92a9a515635410
David <db@d1b.org>
2025-11-04 14:42:37 +1100

    Create kick_problematic_client.sh

---
+++ etc/root/kick_problematic_client.sh
@@ -0,0 +1,13 @@
+#!/bin/sh /etc/rc.common
+MAC="<enter mac address here>"
+IP="<enter ip here>"
+while true; do
+	ping=$(ping $IP-c 4 -W 1 | grep packets | awk '{print $7 }' | sed s/%//g)
+
+	if [ $ping -eq 100 ]
+	then
+		/usr/bin/logger -f KICK_WIFI_CLIENT "deauthing problematic client from wifi"
+		ubus call hostapd.wl0-ap3 del_client "{'addr':'$MAC', 'reason':5, 'deauth':false, 'ban_time':0}"
+	fi
+	sleep 40
+done
